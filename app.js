a2929ea2efa000957eb19f8dbae1b33e555b95be
Chris Owens <guitarrockstarco@gmail.com>
2020-10-27 21:21:29 -0400

    Connect MongoDB and start models

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3333;
+
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = require('./config/db.js');
+connectDB();
+
+const cors = require('cors');
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+};
+
+const app = express();
+
+app.use(cors(corsOptions));
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
